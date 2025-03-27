3c3c96b5044b014a93190d16c3ec6c2d58a756e4
nicolas <niconunes2017@gmail.com>
2025-03-27 20:28:52 -0300

    2703

--- trote/js/scripts.js
+++ trote/js/scripts.js
@@ -1,9 +1,14 @@
 function calcular(){
     //vamos criar 2 variaveis
     //js as variaveis não possuem tipo
-    let mascote, homenagem
+    let mascote, homenagem, leite, kit_avulso, suplemento_avulso, doacao
     mascote = document.getElementById("Mascote").value
     homenagem = document.getElementById("Homenagem").value
-    let soma = Number(mascote) + Number(homenagem)
-    alert(soma)
+    leite = document.getElementById("Leite").value
+    kit_avulso = document.getElementById("Kit_Avulso").value
+    suplemento_avulso = document.getElementById("Suplemento_Avulso").value
+    doacao = document.getElementById("Doacao").value
+    let soma = Number(mascote) + Number(homenagem) + (2 * Number (leite)) + (30 * Number (kit_avulso)) + (15 * Number (suplemento_avulso)) + (20 * Number (doacao))
+    //retorna o valor ao htmll
+    document.getElementById("soma").innerHTML = `A soma é ${soma}`
 }
